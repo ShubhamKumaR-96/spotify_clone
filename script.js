003987eb0672254f88d93b15f9753d3594fc5572
ShubhamKumaR-96 <shubhamkas245@gmail.com>
2024-08-29 22:06:36 +0530

    implement playMusic function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentSong = new Audio();
+
 async function getSongs() {
   let a = await fetch("http://127.0.0.1:5500/songs/");
   let res = await a.text();
@@ -14,18 +16,56 @@ async function getSongs() {
   return songs;
 }
 
+const playMusic = (track) => {
+  currentSong.src = "/songs/" + track;
+  currentSong.play();
+  play.src = "./img/pause.svg";
+};
+
 async function main() {
   // get the lists of all song
   let songs = await getSongs();
   console.log(songs);
 
-  let songsUl=document.querySelector(".songsList").getElementsByTagName("ul")[0];
+  // Show all the songs in the playlist
+  let songsUl = document
+    .querySelector(".songsList")
+    .getElementsByTagName("ul")[0];
   for (const song of songs) {
-     songsUl.innerHTML=songsUl.innerHTML+`<li>${song}</li>`
+    songsUl.innerHTML =
+      songsUl.innerHTML +
+      ` <li>
+                <img class="invert" src="./img/music.svg" alt="music_svg" />
+                <div class="info">
+                  <div>${song.replaceAll("%20copy", "")}</div>
+                  <div> Shubh</div>
+                </div>
+                <div class="playNow">
+                  <span>Play Now</span>
+                  <img class="invert" src="./img/play.svg" alt="plays" />
+                </div></li>`;
   }
 
-  // play the first song
-  var audio = new Audio(songs[0]);
-  audio.play();
+  // Attach an event lister to each song
+  Array.from(
+    document.querySelector(".songsList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      console.log(e.querySelector(".info").firstElementChild.innerHTML);
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  // Attach an event listener to play and previous
+
+  play.addEventListener("click", () => {
+    if (currentSong.pause) {
+      currentSong.play();
+      play.src = "./img/pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "./img/play.svg";
+    }
+  });
 }
 main();
